bc24ca4f2a98519d616ce669938136763f6f3e6b
vudinh158 <vudinhtran2312@gmail.com>
2025-04-13 15:40:15 +0700

    sync chưa

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -57,48 +57,6 @@ const Homepage = () => {
     { thoiGian: "3/2024", nha: "Nhà Q1", phong: "P001", cu: 4312, moi: 6524 },
   ];
 
-<<<<<<< HEAD
-    return (
-        <div style={{display: "flex", width: "100vw", height: "100vh"}}>
-            <div style={{background: '#1B2428', flex: "1 1 20%"}} className="min-h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
-                <div style={{ width: 384, height: 84, left: 0, top: 0, background: '#1B2428', borderBottom: '1px #21373D solid' }}>
-                    <div style={{ width: 363, height: 65, left: 10, top: 9, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 32, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>
-                        <AnimatedSignature text="Quản Lý Nhà Trọ" />
-                    </div>
-                </div>
-                <MainContainer onSelectPage = {setPage} currentPage={page} />
-            </div>
-            <div style={{display: "flex", width: "80%", minHeight: "100vh", background: "#E0E0E0", justifyContent: "center", alignItems: "center"}}>
-                <Header />
-                <SubHeader />
-                <div className='control-content' style={{ width: '80%', height: 835, right: 0, top: 166, position: 'fixed', display: "flex", justifyContent: 'center', alignItems: "center", background: '#E0E0E0', borderBottom: '1px #D2D2D2 solid' }}>
-                    <div style={{width: 'calc(100% - 23px)', height: 815, backgroundColor: 'white', flexDirection: "column", borderRadius: '10px', display: "flex", justifyContent: "space-around", alignItems: "center"}}>
-                        {/* <div style={{ width: 'calc(100% - 23px)', height: 83, right: 0, top: 83, display: "flex", justifyContent: 'flex-start', alignItems: "center", background: 'white', borderBottom: '1px #D2D2D2 solid' }}>
-                            <Button class_name="address-btn btn-2 active-btn" label='1 - 198 ÂU CƠ' />
-                            <Button class_name="address-btn btn-2" label='2 - 123 THANH NIÊN' />
-                            <Button class_name="address-btn btn-2" label='3 - 12 NGUYỄN THÁI HỌC' />
-                        </div> */}
-                        <div style={{ width: 'calc(100% - 23px)', height: "100%", right: 0, top: 83, display: "flex", justifyContent: 'flex-start', borderRadius: '10px', margin: '10px 0', flexDirection: "column", alignItems: "center", background: '#ccc', borderBottom: '1px #D2D2D2 solid' }}>
-                            {page == "home" && (
-                                <Home />
-                            )}
-                            {page == "dien" && (
-                                <DienNuoc type="Điện" data={dataDien} />
-                            )}
-                            {page == "nuoc" && (
-                                <DienNuoc type="Nước" data={dataNuoc} />
-                            )}
-                            {page == "tinhTien" && (
-                                <PaymentIndex />
-                            )}
-                            {page === "dichVu" && (
-                                <DichVuIndex />
-                            )}
-                        </div>
-                    </div>
-                </div>
-            </div>
-=======
   const dataNuoc = [
     { thoiGian: "3/2024", nha: "Nhà Q7", phong: "100A", cu: 821, moi: 845 },
     { thoiGian: "3/2024", nha: "Nhà Q1", phong: "P001", cu: 0, moi: 0 },
@@ -203,7 +161,6 @@ const Homepage = () => {
           >
             <AnimatedSignature text="Quản Lý Nhà Trọ" />
           </div>
->>>>>>> 4d6b96536efd4acb2f90df352634c6dc34f1aef2
         </div>
         <MainContainer onSelectPage={setPage} currentPage={page} />
       </div>
